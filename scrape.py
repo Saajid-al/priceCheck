f307710bd532a1dec721695dc73885ed9a178fdf
Saajid <Saajid.aliyar@outlook.com>
2021-01-09 06:23:36 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,10 +7,10 @@ while "https://www.newegg.ca/" not in URL:
     URL = input("Enter a link : ")
 SuggestedPrice = input("Please enter a price to watch for : ")
 
-#URL = "https://www.newegg.ca/p/34-725-086"
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+#URL = "https://www.newegg.ca/p/34-725-086" #EXAMPLE LINK
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"} 
 
-def priceCheck():
+def priceCheck(): #PARSING THROUGH THE WEB CONTENT
     webpage = requests.get(URL, headers = headers) 
     soup = BeautifulSoup(webpage.content, "html.parser")
     Product = soup.find(class_="product-title").get_text()
@@ -19,24 +19,24 @@ def priceCheck():
     rounded = rounded.replace(',','')
     roundedPrice = float(rounded)
 
-    if (roundedPrice < int(SuggestedPrice)):
+    if (roundedPrice < int(SuggestedPrice)): #CHECKING IF PRICE DIPS BELOW PREFFERED
         sendMail()
         print(Product.strip())
         print(cost.strip())
         
-def sendMail():
+def sendMail(): #SENDING MAIL FUNCTION
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login("Fakespamemail22@gmail.com", "789123gG+")
+    server.login("CHANGE WITH PREFERRED EMAIL", "CHANGE WITH PASSWORD")
     subject = "price drop!"
     body = "The Price has Dropped! Here is the Link : ", URL
     message = f"Subject: {subject}\n\n{body}"
     
     server.sendmail(
-        "Fakespamemail22@gmail.com",
-        "Fakespamemail22@gmail.com",
+        "CHANGE WITH PREFERRED EMAIL", #FROM 
+        "CHANGE WITH PREFERRED EMAIL", #SEND TO 
         message
     )
     print("EMAIL SENT")
